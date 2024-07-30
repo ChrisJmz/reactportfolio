60a411f8b59c059c2c72e40e098805bf65ce0990
ChrisJmz <christianjimenezgf@gmail.com>
2024-07-30 20:19:18 +0200

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  assetPrefix: isProd ? '/portfolio/' : '',
+  basePath: isProd ? '/portfolio' : '',
+  output:'export',
+};
 
 export default nextConfig;
